503cf671129bc3876f316299d8733d17a5220b1f
Dan Johnson <danieljohnson2@verizon.net>
2018-02-03 04:56:02 -0500

    Initial comment; first effort

---
+++ playerlist.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+from sys import argv
+from os import chdir, getcwd
+from os.path import isdir, isfile, basename
+from glob import iglob
+from fnmatch import fnmatch
+
+extensions = ['*.mp3', '*.wma']
+
+def isvalidfile(name):
+    if isfile(name):
+        for pat in extensions:
+            if fnmatch(name.lower(), pat):
+                return True
+    return False
+
+for dir in argv[1:]:
+    initial_path = getcwd()
+    if isdir(dir) and not dir.startswith('.'):
+        playlist_name = basename(dir) + ".m3u"
+        print(playlist_name)
+                
+        chdir(dir)
+        
+        with open(playlist_name, "w") as f:
+            for filename in iglob("**/*", recursive=True):
+                if isvalidfile(filename):
+                    f.write(filename + '\n')
+
+        chdir(initial_path)
+
